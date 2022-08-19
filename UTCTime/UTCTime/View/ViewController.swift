8ef103ec981f6bf3231c73af85acc4ed98168a46
Jimin Kim <0305jimin@gmail.com>
2022-08-20 00:18:43 +0900

    Chore: installing Rx* libraries #1

--- UTCTime/UTCTime/View/ViewController.swift
+++ UTCTime/UTCTime/View/ViewController.swift
@@ -32,22 +32,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //콜백 세팅
-        viewModel.onUpdated = {[weak self] in
-            
-            DispatchQueue.main.async {
-                self?.datetimeLabel.text = self?.viewModel.dateTimeString
-            }
-            //DispatchQueue.main.async 뷰모델의 didSet부분에 쓰려고 했지만, UIKit을 사용하는 이 파일에 두는 것이 더 낫다.
-            viewModel.dateTimeString //변형이 일어나면
+        viewModel.dateTimeString //변형이 일어나면
 //                .observe(on: MainScheduler.instance)
 //                .subscribe(onNext:{ str in//스트링이 들어오면
 //                    self.datetimeLabel.text = str
 //                })
 //                .disposed(by: disposeBag)
-                .bind(to: datetimeLabel.rx.text)
-                .disposed(by: disposeBag)
-        }
+            .bind(to: datetimeLabel.rx.text)
+            .disposed(by: disposeBag)
+        
         viewModel.reload()//fetchNow를 하고 모델로 변형된 다음 화면으로 보이기 위한 스트링 타입으로 최종 변환까지 된다.
     }
 }
